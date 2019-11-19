cc6b74d4b9fe46976495685c2a2c74b6ead057e0
Daniel Szego <szego.daniel@gmail.com>
2019-11-19 06:38:19 -0800

    modofied ca reachability

--- balancetracker-qa-kubernetes/scripts/users.sh
+++ balancetracker-qa-kubernetes/scripts/users.sh
@@ -9,7 +9,7 @@ echo "##### Enroll fabric ca admin user #########"
 echo "#####################################################"
 echo
 
-fabric-ca-client enroll -u https://Interticket:Blockchain4ever@localhost:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+fabric-ca-client enroll -u https://Interticket:Blockchain4ever@ca:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
 
 echo
 echo "#####################################################"
@@ -17,7 +17,7 @@ echo "##### Create client admin #########"
 echo "#####################################################"
 echo
 
-fabric-ca-client register --id.name clientadmin --id.secret Blockchain4ever --id.affiliation org1 --id.attrs 'hf.Revoker=true,admin=true:ecert' -u https://localhost:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+fabric-ca-client register --id.name clientadmin --id.secret Blockchain4ever --id.affiliation org1 --id.attrs 'hf.Revoker=true,admin=true:ecert' -u https://ca:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
 
 echo
 echo "#####################################################"
@@ -25,7 +25,7 @@ echo "##### Create exploreradmin #########"
 echo "#####################################################"
 echo
 
-fabric-ca-client register --id.name exploreradmin --id.secret Blockchain4ever --id.affiliation org1 --id.attrs 'hf.Revoker=true,admin=true:ecert' -u https://localhost:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+fabric-ca-client register --id.name exploreradmin --id.secret Blockchain4ever --id.affiliation org1 --id.attrs 'hf.Revoker=true,admin=true:ecert' -u https://ca:7054 --tls.certfiles /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
 
 echo "##########################################"
 echo "##### End of users #########"
