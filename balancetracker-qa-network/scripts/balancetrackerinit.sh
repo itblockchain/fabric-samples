1dd8ff0722f76f0aabe6a2e00ae0d78dd6dbcf92
dsz <szego.daniel@gmail.com>
2020-03-21 16:38:10 +0100

    fix in integration test script

--- balancetracker-qa-network/scripts/balancetrackerinit.sh
+++ balancetracker-qa-network/scripts/balancetrackerinit.sh
@@ -366,7 +366,7 @@ sleep 2
 echo
 echo
 
-peer chaincode query -C mychannel -n mycc -c '{"Args":["GetQueryResult","{\"selector\":{\"quorum\":0,\"modelType\":\"Account\",  \"tags\": {\"testTag1\": \"testTag1\"}}}"]}'
+peer chaincode query -C mychannel -n bccc -c '{"Args":["GetQueryResult","{\"selector\":{\"quorum\":0,\"modelType\":\"Account\",  \"tags\": {\"testTag1\": \"testTag1\"}}}"]}'
 sleep 2
 
 echo
